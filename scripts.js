fbaa922aa5ac9484a8b3a086e6637b2ea3e36fd3
Martin2005F <martinbosancic1@gmail.com>
2025-07-07 21:10:36 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -272,3 +272,11 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener("scroll", handleScrollAnimations);
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+  navLinks.classList.toggle("open");
+});
